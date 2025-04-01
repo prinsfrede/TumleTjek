f6417d99b81a2b2cd5b974317780c91476ea34bc
prinsfrede <frederikjensen@hotmail.dk>
2025-04-01 18:13:12 +0200

    Added Some Repos

---
+++ TumleTjek/Repo/AktivitetRepo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TumleTjek.Model;
+
+namespace TumleTjek.Repo
+{
+    public class AktivitetRepo
+    {
+        private List<Aktivitet> aktiviteter = new List<Aktivitet>();
+    }
+}
+ 
---
+++ TumleTjek/Repo/BarnRepo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TumleTjek.Model;
+
+namespace TumleTjek.Repo
+{
+    public class BarnRepo
+    {
+
+        private List<Barn> barns = new List<Barn>();
+
+    }
+}
