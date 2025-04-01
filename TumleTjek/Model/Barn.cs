1ff893338c0017bf1bda5978e6c10b9762d0acc7
Frederik Jensen <frederikkjensen@hotmail.dk>
2025-04-01 10:29:07 +0200

    Added Basic Classes

---
+++ TumleTjek/Model/Barn.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TumleTjek.Model
+{
+    public class Barn
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        public Forældre forældre { get; set; }
+
+        public bool IsSick { get; set; }
+    }
+}
---
+++ TumleTjek/Model/Medarbejder.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TumleTjek.Model
+{
+    public class Medarbejder
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+      
+    }
+}
