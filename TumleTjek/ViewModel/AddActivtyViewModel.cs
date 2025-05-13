e776ebff5732687e495dfc62adec325b2a45cfdd
Apla40 <andreaslacor@gmail.com>
2025-05-13 10:37:42 +0200

    Add AddActivtyViewModel og UI til AddActivtyView

---
+++ TumleTjek/ViewModel/AddActivtyViewModel.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TumleTjek.ViewModel
+{
+    public class AddActivtyViewModel : BaseViewModel
+    {
+        private string _activityName;
+        private string _activityDescription;
+        private string _activityDate;
+        private string _activityTime;
+        public string ActivityName
+        {
+            get => _activityName;
+            set
+            {
+                _activityName = value;
+                OnPropertyChanged(nameof(ActivityName));
+            }
+        }
+        public string ActivityDescription
+        {
+            get => _activityDescription;
+            set
+            {
+                _activityDescription = value;
+                OnPropertyChanged(nameof(ActivityDescription));
+            }
+        }
+        public string ActivityDate
+        {
+            get => _activityDate;
+            set
+            {
+                _activityDate = value;
+                OnPropertyChanged(nameof(ActivityDate));
+            }
+        }
+        public string ActivityTime
+        {
+            get => _activityTime;
+            set
+            {
+                _activityTime = value;
+                OnPropertyChanged(nameof(ActivityTime));
+            }
+        }
+    
+    }
+}
